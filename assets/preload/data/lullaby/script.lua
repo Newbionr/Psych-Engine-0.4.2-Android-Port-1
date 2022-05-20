caf885c53823cb2dd5f35c0b4efef04b77df9c8f
Newbionr <101176446+Newbionr@users.noreply.github.com>
2022-05-20 17:52:02 +0900

    Add files via upload

--- assets/preload/data/lullaby/script.lua
+++ assets/preload/data/lullaby/script.lua
@@ -1,93 +1,43 @@
-local allowCountdown = false
 local keepScroll = false
-function onCreate()
+local skipCountdown = true
+
+function onCreate() 
+	setProperty('title.alpha', 0);
+	setProperty('camGame.alpha', 0);
 	setProperty('camHUD.alpha', 0);
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'golddeath');
+	setProperty('sleep.alpha', 0);
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bonusdeath');
 	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx');
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', '');
 	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
 end
-function onStartCountdown()
-	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
-	if not allowCountdown and isStoryMode and not seenCutscene then
-		setProperty('inCutscene', true);
-		runTimer('startDialogue', 0.8);
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
-function onTimerCompleted(tag, loops, loopsLeft)
-	if tag == 'startDialogue' then -- Timer completed, play dialogue
-		startDialogue('dialogue', '');
-	end
-end
 function onUpdate()
-    if curStep == 0 then
-	doTweenAlpha('hud', 'camHUD', 1, 0.5, "quartInOut")
+    if curStep == 4 then
+	doTweenAlpha('title', 'title', 1, 3, "quartInOut")
+	noteTweenAlpha("NoteAlpha5", 4, 0.4, transitionTime, linear)
+	noteTweenAlpha("NoteAlpha6", 5, 0.4, transitionTime, linear)
+	noteTweenAlpha("NoteAlpha7", 6, 0.4, transitionTime, linear)
+	noteTweenAlpha("NoteAlpha8", 7, 0.4, transitionTime, linear)
 	end
 end
 function onStepHit()
-	if curStep == 1 then
-		setPropertyFromGroup('opponentStrums', 0, 'x', defaultOpponentStrumX0 - 700)
-		setPropertyFromGroup('opponentStrums', 1, 'x', defaultOpponentStrumX1 - 700)
-		setPropertyFromGroup('opponentStrums', 2, 'x', defaultOpponentStrumX2 - 700)
-		setPropertyFromGroup('opponentStrums', 3, 'x', defaultOpponentStrumX3 - 700)
-		setPropertyFromGroup('playerStrums', 0, 'x', defaultOpponentStrumX0)
-		setPropertyFromGroup('playerStrums', 1, 'x', defaultOpponentStrumX1)
-		setPropertyFromGroup('playerStrums', 2, 'x', defaultOpponentStrumX2)
-		setPropertyFromGroup('playerStrums', 3, 'x', defaultOpponentStrumX3)
-		doTweenX('gold2', 'gold2', 0, 0.5, "quartInOut")
+    if curStep == 30 then
+		doTweenX('gfjeff', 'gfjeff', 0, 0.5, "quartInOut")
 	end
-	if curStep == 18 then
-		doTweenAlpha('gold2', 'gold2', 0, 1, "quartInOut")
+	if curStep == 50 then
+		doTweenAlpha('gfjeff', 'gfjeff', 0, 1, "quartInOut")
     end
-	if curStep == 832 then
-	cameraFlash('other', 0xff000000, 1,false)
-	doTweenAlpha('game', 'camGame', 1, 1, "quartInOut")
-	doTweenAlpha('hud', 'camHUD', 0.6, 1, "quartInOut")
-	setProperty("blurrybgred.visible",false)
-	setProperty("platforms.visible",false)
-	setProperty("timeTxt.visible",false)
-	setProperty("scoreTxt.visible",false)
-	setProperty("healthBar.visible",false)
-	setProperty("healthBarBG.visible",false)
-	setProperty("uibars.visible",false)
-	setProperty("celebi.visible", false);
-	setProperty("cyndaquil.visible", false);
+	if curStep == 20 then
+		doTweenAlpha('title', 'title', 0, 5, "quartInOut")
+		doTweenAlpha('game', 'camGame', 1, 5, "quartInOut")
+		doTweenAlpha('hud', 'camHUD', 1, 5, "quartInOut")
 	end
-	if curStep == 960 then
-	cameraFlash('other', 0xff000000, 1,false)
-	doTweenAlpha('hud', 'camHUD', 1, 1, "quartInOut")
-	setProperty("blurrybgred.visible",true)
-	setProperty("platforms.visible",true)
-	setProperty("timeTxt.visible",true)
-	setProperty("scoreTxt.visible",true)
-	setProperty("healthBar.visible",true)
-	setProperty("healthBarBG.visible",true)
-	setProperty("uibars.visible",true)
-	setProperty("celebi.visible", true);
-	setProperty("cyndaquil.visible", true);
+	if curStep == 1151 then
+		doTweenAlpha('title', 'title', 0, 0.8, "quartInOut")
+		doTweenAlpha('game', 'camGame', 0, 0.8, "quartInOut")
+		doTweenAlpha('hud', 'camHUD', 0, 0.8, "quartInOut")
 	end
-end
-function opponentNoteHit()
-    health = getProperty('health')
-    if getProperty('health') > 0.4 then
-        setProperty('health', health- 0.017);
-    end
-end
-function goodNoteHit(id, noteData, noteType, isSustainNote)
-	if noteType == 'Psychic Note' then
-		objectPlayAnimation('celebi', 'attack', false)
-		cameraFlash('hud', 0xFFFFFFFF, 5,false)
-		playSound("confusion", 0.7)
-	end
-end
-function onBeatHit()
-	if curBeat % 2 == 0 then 
-	   objectPlayAnimation('celebi', 'loop', true);
-	end
-	if curBeat % 2 == 0 then 
-	   objectPlayAnimation('cyndaquil', 'loop', true);
+	if curStep == 1166 then
+		setProperty('sleep.alpha', 1);
 	end
 end
